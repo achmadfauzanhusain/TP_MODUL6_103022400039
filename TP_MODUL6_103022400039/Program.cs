13d6ef2a7014619172a931f9d6242bc078a674cb
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-04-05 10:45:42 +0800

    commit message

--- TP_MODUL6_103022400039/Program.cs
+++ TP_MODUL6_103022400039/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+        this.playCount = 0;
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("=== Track Details ===");
+        Console.WriteLine("ID        : " + id);
+        Console.WriteLine("Title     : " + title);
+        Console.WriteLine("PlayCount : " + playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack track = new SayaMusicTrack("Lagu Favorit");
+
+        track.IncreasePlayCount(5);
+        track.PrintTrackDetails();
+    }
+}
--- TP_MODUL6_103022400039/obj/Debug/net10.0/TP_MODUL6_103022400039.AssemblyInfo.cs
+++ TP_MODUL6_103022400039/obj/Debug/net10.0/TP_MODUL6_103022400039.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400039")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f0b6b6caa4928651c5cb8a1edbd17deb9f96a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400039")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400039")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
